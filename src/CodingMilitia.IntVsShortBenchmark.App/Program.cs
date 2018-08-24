56d809a60e538e39bdfbe43d6bb92c0813457d97
João Antunes <joaofbantunes@gmail.com>
2018-08-24 22:04:38 +0100

    Adding project to source control.

---
+++ src/CodingMilitia.IntVsShortBenchmark.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+
+namespace CodingMilitia.IntVsShortBenchmark.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<IntVsShort>(
+                ManualConfig.Create(DefaultConfig.Instance).With(MemoryDiagnoser.Default)
+            );
+        }
+    }
+
+    public class IntVsShort
+    {
+        private const short Iterations = short.MaxValue;
+
+        [Benchmark(Baseline = true)]
+        public void ForUsingInt()
+        {
+            for (int i = 0; i < Iterations; ++i)
+            {
+            }
+        }
+
+        [Benchmark]
+        public void ForUsingShort()
+        {
+            for (short i = 0; i < Iterations; ++i)
+            {
+            }
+        }
+    }
+}
